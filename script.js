55f2ae4c8492e137a2278bfe42a013a137672df6
Code Malayalam <ktucsnotes@gmail.com>
2021-01-05 15:23:15 +0000

    Added youtube api

---
+++ script.js
@@ -0,0 +1,72 @@
+
+
+const initiateApi = () => {
+    return fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyBSKzus6IHW_j6rNIjSoXm4Rz5IOAlr4tI&channelId=UC6VX0uvoQekFE5Lc6vVywdw&part=snippet,id&order=date&maxResults=100')
+    .then(response => response.json())
+    .then(data => data);
+}
+
+const doMatch = (a, b) => {
+    return a.toLowerCase().indexOf(b.toLowerCase()) !== -1;
+}
+
+const template = (item) => {
+    const title = item.snippet.title;
+    const id = item.id.videoId;
+    return `
+<div class="box">
+<div class="box-title">
+    ${title}
+</div>
+<div class="box-link">
+    <a href="http://www.youtube.com/watch?v=${id}}" target="_blank" rel="noopener noreferrer">See in Youtube</a>
+</div>
+</div>
+    `;
+}
+
+
+const fillDom = (arr, id) => {
+    const dom = arr.map(template).join(' ');
+    var node = document.getElementById(id);
+    node.insertAdjacentHTML('beforeend', dom);
+}
+const main = () => {
+    initiateApi()
+        .then((data) => {
+            const jsArr = [];
+            const gitArr = [];
+            const reactArr = [];
+            const othersArr = [];
+
+            const items = data.items;
+            items.forEach(item => {
+                if(item.id.videoId) {
+                    if(doMatch(item.snippet.title, 'JavaScript')) {
+                        jsArr.push(item);
+                    } else if (doMatch(item.snippet.title, 'Git')) {
+                        gitArr.push(item);
+                    } else if (doMatch(item.snippet.title, 'React')) {
+                        reactArr.push(item);
+                    } else {
+                        othersArr.push(item);
+                    }
+                }
+            });
+
+            console.log(jsArr);
+            console.log(gitArr);
+            console.log(othersArr);
+
+            const content = document.getElementById('loading');
+            content.classList.add('hide-loading');
+
+            fillDom(gitArr.reverse(), 'git');
+            fillDom(othersArr.reverse(), 'others');
+            fillDom(jsArr.reverse(), 'js');
+            
+
+        })
+}
+
+main();
